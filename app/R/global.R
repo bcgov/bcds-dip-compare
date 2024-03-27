59f9035b3ef0e4819e1feda570ca649706d0a535
Lindsay Fredrick <Lindsay.Fredrick@gov.bc.ca>
2024-03-27 14:26:04 -0700

    add combined files to global for easier read in

--- app/R/global.R
+++ app/R/global.R
@@ -26,5 +26,7 @@ library(DT)         ## for tables
 ## Add code that you want to run before your app launches
 
 ### e.g., reading in data
-
-
+combined_overview <- read_csv(safepaths::use_network_path("2023 ARDA BCDS Data Evaluation/data_for_dashboard/combined/combined_overview.csv"))
+combined_detailed <- read_csv(safepaths::use_network_path("2023 ARDA BCDS Data Evaluation/data_for_dashboard/combined/combined_detailed.csv"))
+combined_list_vars <- read_csv(safepaths::use_network_path("2023 ARDA BCDS Data Evaluation/data_for_dashboard/combined/combined_list.csv"))
+combined_summary <- read_csv(safepaths::use_network_path("2023 ARDA BCDS Data Evaluation/data_for_dashboard/combined/combined_summary.csv"))
